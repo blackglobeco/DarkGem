b705f4a4cde3c6fe77e3ffb44d28a8b0d1ea34a4
Black Globe <137195478+blackglobeco@users.noreply.github.com>
2025-01-18 00:46:27 +0800

    Update ConfigManager.py

--- src/Manager/ConfigManager.py
+++ src/Manager/ConfigManager.py
@@ -8,7 +8,7 @@ class ConfigManager:
     """
 
     CONFIG_FILE_PATH = "config.yml"
-    DEFAULT_GEMINI_MODEL_ID = "gemini-pro"
+    DEFAULT_GEMINI_MODEL_ID = "gemini-1.5-flash"
 
     @staticmethod
     def load_config() -> Optional[Dict[str, Any]]:
